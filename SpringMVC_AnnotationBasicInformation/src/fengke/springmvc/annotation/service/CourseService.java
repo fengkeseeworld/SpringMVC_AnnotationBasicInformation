30c82776d22a29143539aa5181d22f37f9c11e26
fengkeseeworld <fengkeseeworld@163.com>
2015-11-02 01:40:35 +0800

    success

---
+++ SpringMVC_AnnotationBasicInformation/src/fengke/springmvc/annotation/service/CourseService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package fengke.springmvc.annotation.service;
+
+import fengke.springmvc.annotation.model.Course;
+
+/**
+ * @author ·æ¿Í
+ *
+ */
+public interface CourseService {
+	
+	Course getCoursebyId(int cid);
+	
+
+}
